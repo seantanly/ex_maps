c28df18177e2276e3f645dcdc1d2369ff834f60f
Sean Tan <sean.tan.ly@gmail.com>
2018-08-29 02:47:29 +0800

    fix(response): retry JSON decoding with Poison when Jason fails

--- lib/google_maps/response.ex
+++ lib/google_maps/response.ex
@@ -3,15 +3,34 @@ defmodule GoogleMaps.Response do
 
   @type t :: {:ok, map()} | {:error, error()} | {:error, error(), String.t()}
 
-  @type status :: String.t
+  @type status :: String.t()
 
-  @type error :: HTTPoison.Error.t | status()
+  @type error :: HTTPoison.Error.t() | status()
 
   def wrap({:error, error}), do: {:error, error}
+
   def wrap({:ok, %{body: body} = response}) when is_binary(body) do
-    wrap({:ok, %{response | body: Jason.decode!(body)}})
+    binary = IO.iodata_to_binary(body)
+
+    decoded_body =
+      Jason.decode(body, strings: :copy)
+      |> case do
+        {:ok, body} ->
+          body
+
+        {:error, error} ->
+          IO.puts("[#{__MODULE__}]: #{inspect(error)}\n#{inspect(body)}")
+          IO.puts(binary)
+          Poison.decode!(body)
+      end
+
+    wrap({:ok, %{response | body: decoded_body}})
   end
+
   def wrap({:ok, %{body: %{"status" => "OK"} = body}}), do: {:ok, body}
-  def wrap({:ok, %{body: %{"status" => status, "error_message" => error_message}}}), do: {:error, status, error_message}
+
+  def wrap({:ok, %{body: %{"status" => status, "error_message" => error_message}}}),
+    do: {:error, status, error_message}
+
   def wrap({:ok, %{body: %{"status" => status}}}), do: {:error, status}
 end
